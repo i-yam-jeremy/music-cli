9407ae2ad515e472f108832990ee5f9fe59c148d
i-yam-jeremy <i-yam-jeremy@users.noreply.github.com>
2018-08-30 20:09:59 -0700

    Can now play songs in playlist and it works

--- music-cli.sh
+++ music-cli.sh
@@ -1,5 +1,7 @@
 MUSIC_CLI_DIR=~/.music-cli
 ACTIVE_PLAYLIST_FILE=$MUSIC_CLI_DIR/active_playlist
+bold=`tput bold`
+normal=`tput sgr0`
 OP=$1
 if [ ! -d $MUSIC_CLI_DIR ]; then
 	mkdir $MUSIC_CLI_DIR
@@ -15,10 +17,21 @@ if [ -n $OP ]; then
 		SONG_NAME=$2
 		ARTIST_NAME=$3
 		YOUTUBE_URL=$4
-		FILENAME=$SONG_NAME-$ARTIST_NAME.mp3
+		FILENAME=`ls -1 $MUSIC_CLI_DIR/$ACTIVE_PLAYLIST | wc -l`.wav
 		FILEPATH=$MUSIC_CLI_DIR/$ACTIVE_PLAYLIST/$FILENAME
-		youtube-dl --extract-audio --audio-format mp3 -o $FILEPATH $YOUTUBE_URL
-		echo $FILENAME >> $MUSIC_CLI_DIR/$ACTIVE_PLAYLIST/playlist_data
+		youtube-dl --extract-audio --audio-format wav -o $FILEPATH $YOUTUBE_URL 
+		printf "${SONG_NAME}\n${ARTIST_NAME}\n${FILENAME}\n" >> $MUSIC_CLI_DIR/$ACTIVE_PLAYLIST/playlist_data
+	elif [ $OP = "list" ]; then
+		FILES=`ls -1 $MUSIC_CLI_DIR`
+		while read -r FILE; do
+			if [ $FILE != "active_playlist" ]; then
+				if [ $FILE == $ACTIVE_PLAYLIST ]; then
+					echo $bold$FILE$normal
+				else
+					echo $FILE
+				fi
+			fi
+		done <<< "$FILES"
 	elif [ $OP = "active" ]; then
 		if [ -z $2 ]; then
 			if [ -z $ACTIVE_PLAYLIST ]; then
@@ -33,9 +46,15 @@ if [ -n $OP ]; then
 		PLAYLIST_NAME=$2
 		mkdir $MUSIC_CLI_DIR/$PLAYLIST_NAME
 		echo -n > $MUSIC_CLI_DIR/$PLAYLIST_NAME/playlist_data
-	elif [ $OP = " " ]; then
-		echo "Test"
+	elif [ $OP = "play" ]; then
+		while read -r SONG_FILE; do
+			if [[ $SONG_FILE =~ .*?\.wav$ ]]; then
+				SONG_FILE_PATH=$MUSIC_CLI_DIR/$ACTIVE_PLAYLIST/$SONG_FILE
+				mplayer $SONG_FILE_PATH > /dev/null
+			else
+				echo $SONG_FILE
+			fi
+		done < $MUSIC_CLI_DIR/$ACTIVE_PLAYLIST/playlist_data
 	fi
-
 fi
 
